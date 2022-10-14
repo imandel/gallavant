f126da8d6814453b7c1431dfc07f5860a4be7dba
JP <jorgeluis.pardogaytan@hdr.qut.edu.au>
2022-10-14 11:40:48 -0400

    This version is running and we were able to add a 'red box' to test adding a new element to the interface

---
+++ src/Plots.svelte
@@ -0,0 +1,15 @@
+<script>
+import {plots} from './stores';
+
+console.log(plots)
+</script>
+
+<div class='dummy'>{$plots}</div>
+
+<style>
+.dummy{
+height:300px;
+width: 300px;
+background-color: red;
+}
+</style>
